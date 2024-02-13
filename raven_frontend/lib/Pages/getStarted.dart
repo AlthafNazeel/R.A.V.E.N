6bd6ef501ef4104d18f4ae0038f71c5b7808039e
Oshadhi <oshadhirajakaruna2005@gmail.com>
2024-02-13 15:28:41 +0530

    commit 13th feb

--- raven_frontend/lib/Pages/getStarted.dart
+++ raven_frontend/lib/Pages/getStarted.dart
@@ -8,36 +8,61 @@ class GetStartedPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Get Started'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to Your App!',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+      backgroundColor: Color.fromARGB(255, 38, 32, 49), // Set background color to deep purple
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome to R.A.V.E.N!',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // Set text color to white
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Join the movement towards a safer future',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white, // Set text color to white
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20.0),
-            Text(
-              'Get started by tapping below',
-              style: TextStyle(
-                fontSize: 18.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignIn()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(89, 66, 118, 1), // Set button color to purple
+                  padding: EdgeInsets.symmetric(horizontal: 60, vertical: 15), // Adjust padding
+                ),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.white, // Set text color to white
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 40.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignIn()),
-                );
-              },
-              child: Text('Continue to Sign In'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
