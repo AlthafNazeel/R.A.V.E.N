a208d328fd185697bda53ed66076aff60e88511d
Oshadhi <oshadhirajakaruna2005@gmail.com>
2024-03-04 15:28:41 +0530

    pull test

--- raven_frontend/lib/Pages/getStarted.dart
+++ raven_frontend/lib/Pages/getStarted.dart
@@ -1,44 +1,57 @@
-// import 'package:flutter/material.dart';
-// import 'package:raven_frontend/Pages/signIn.dart';
+import 'package:flutter/material.dart';
 
-// class GetStartedPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Get Started'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               'Welcome to Your App!',
-//               style: TextStyle(
-//                 fontSize: 24.0,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//             SizedBox(height: 20.0),
-//             Text(
-//               'Get started by tapping below',
-//               style: TextStyle(
-//                 fontSize: 18.0,
-//               ),
-//             ),
-//             SizedBox(height: 40.0),
-//             ElevatedButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(builder: (context) => SignInp()),
-//                 );
-//               },
-//               child: Text('Continue to Sign In'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class GetStartedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('raven_frontend/images/getstarted bg.jpg'), 
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'raven_frontend/images/ravenLogo.png', 
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "JOIN THE MOVEMENT TOWARDS A SAFER FUTURE",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your logic here for the button action
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  child: Text(
+                    "GET STARTED",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // Replace with your desired button color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
