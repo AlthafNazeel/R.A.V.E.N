f337b58a006477cbde319965de713a7e1a9cc1a3
Tufayl <tufaylasaf@gmail.com>
2024-03-14 14:47:48 +0530

    Firebase API

--- raven_frontend/lib/api/firebase_api.dart
+++ raven_frontend/lib/api/firebase_api.dart
@@ -1,15 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:raven_frontend/main.dart';
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
+  final db = FirebaseFirestore.instance;
+
+  var fCMToken;
 
   Future<void> initNotification() async {
     await _firebaseMessaging.requestPermission();
 
-    final fCMToken = await _firebaseMessaging.getToken();
+    fCMToken = await _firebaseMessaging.getToken();
 
     print("Token $fCMToken");
+
+    // await db.collection("Notifications").get().then((event) {
+    //   for (var doc in event.docs) {
+    //     print("${doc.id} => ${doc.data()}");
+    //   }
+    // });
+
+    // addSystem("ADiWRUE96Mjyzgx41HHh", "dsadsa");
+    print(getNotificationData("ADiWRUE96Mjyzgx41HHh"));
 
     initPushNotifications();
   }
@@ -25,5 +38,46 @@ class FirebaseApi {
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
 
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+  }
+
+  void addSystem(String serverID, String pwd) async {
+    try {
+      DocumentSnapshot documentSnapshot =
+          await db.collection("Servers").doc(serverID).get();
+
+      if (documentSnapshot.exists) {
+        if (documentSnapshot.get("password") == pwd) {
+          await db.collection("Servers").doc(serverID).update({
+            "tokenIDs": FieldValue.arrayUnion([fCMToken]),
+          });
+          print("${documentSnapshot.id} => ${documentSnapshot.data()}");
+        } else {
+          print("Password does not match!");
+        }
+      } else {
+        print("Document does not exist");
+      }
+    } catch (error) {
+      print('Error getting document: $error');
+    }
+  }
+
+  Future<List<dynamic>> getNotificationData(String serverID) async {
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection("Notifications")
+          .where("serverID", isEqualTo: serverID)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.map((doc) => doc.data()).toList();
+      } else {
+        print("No documents found with serverID: $serverID");
+        return [];
+      }
+    } catch (error) {
+      print('Error getting notification data: $error');
+      return [];
+    }
   }
 }
--- raven_frontend/lib/main.dart
+++ raven_frontend/lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:raven_frontend/Pages/getStarted.dart';
 
-import 'package:flutter/rendering.dart';
 import 'package:raven_frontend/Pages/alert.dart';
-import 'package:raven_frontend/Pages/getStarted.dart';
 import 'package:raven_frontend/Pages/signInPage.dart';
 import 'package:raven_frontend/Pages/homePage.dart';
 import 'package:raven_frontend/Pages/add_device.dart';
@@ -12,10 +11,12 @@ import 'package:raven_frontend/Pages/Notifications.dart';
 import 'package:raven_frontend/api/firebase_api.dart';
 import 'package:raven_frontend/firebase_options.dart';
 
-
 final navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotification();
   runApp(MyApp());
 }
 
@@ -27,10 +28,10 @@ class MyApp extends StatelessWidget {
       home: GetStartedPage(),
       navigatorKey: navigatorKey,
       routes: {
-        '/notification_screen': (context) => const Alert(),
-        '/SettingsPage': (context) =>  Settings(),
-        '/signInPage': (context) =>  SignIn(),
-        '/homePage': (context) =>  HomePage(),
+        '/notification_screen': (context) => const VideoApp(),
+        '/SettingsPage': (context) => Settings(),
+        '/signInPage': (context) => SignIn(),
+        '/homePage': (context) => HomePage(),
         '/add_device': (context) => const AddDevice(),
         '/GetStartedPage': (context) => GetStartedPage(),
         '/Notifications': (context) => NotificationsPage(),
