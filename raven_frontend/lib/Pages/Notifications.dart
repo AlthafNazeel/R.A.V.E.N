7ebd41062e399556ee83710a61614e198357faf7
Althaf Nazeel <althafnazeel8@gmail.com>
2024-03-17 20:35:22 +0530

    Successfully extracted Notification data from the database

--- raven_frontend/lib/Pages/Notifications.dart
+++ raven_frontend/lib/Pages/Notifications.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:raven_frontend/api/firebase_api.dart'; // Import FirebaseApi
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,148 +12,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotificationsPage(),
+      home: NotificationsPageE(),
     );
   }
 }
 
-class NotificationsPage extends StatefulWidget {
+class NotificationsPageE extends StatefulWidget {
   @override
   _NotificationsPageState createState() => _NotificationsPageState();
 }
 
-class _NotificationsPageState extends State<NotificationsPage> {
-
-  String selectedFilter = 'All';
-
-  void onFilterSelected(String filter) {
-    setState(() {
-      selectedFilter = filter;
-    });
-  }
+class _NotificationsPageState extends State<NotificationsPageE> {
+  final FirebaseApi api = FirebaseApi(); // Initialize FirebaseApi
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Center(
-          child: Text('Notifications         ',
-          style: TextStyle(color: Colors.white),
+          child: Text(
+            'Notifications',
+            style: TextStyle(color: Colors.white),
           ),
         ),
-
         backgroundColor: Color.fromARGB(255, 41, 29, 46),
-
         leading: IconButton(
           icon: Icon(Icons.menu, color: Colors.white),
-          onPressed: (){
-            Navigator.pushNamed(context, '/homePage');
+          onPressed: () {
+            // code to open menu
           },
         ),
       ),
-      
-      body: ListView(
-        children: 
-        
-        <Widget>[
+      body: FutureBuilder(
+        future: api.getNotificationData("ADiWRUE96Mjyzgx41HHh"), // serverID 
+        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            List<dynamic> notifications = snapshot.data!;
+            return ListView.builder(
+              itemCount: notifications.length,
+              itemBuilder: (context, index) {
+                final notification = notifications[index];
+                final title = notification['title'] ?? 'No Title';
+                final subtitle = notification['subtitle'] ?? 'No Subtitle';
+                // final time = notification['time'] ?? 'No Time'; 
+                final time = notification['time'] != null
+                // ? DateFormat('yyyy-MM-dd HH:mm:ss').format(notification['time'].toDate())
+                ? DateFormat('hh:mm:ss').format(notification['time'].toDate())
+                : 'No Time';
 
-          
-
-          Padding(
-            padding: const EdgeInsets.only(top: 15, left: 17),
-            child: Text(
-              'Today',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-
-          // Notification tiles for each notification
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Motion detected',
-            subtitle: 'Camera 03',
-            time: '14:23',
-            isRead: false,
-            onTap: () {},
-          ),
-
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Crime detected',
-            subtitle: 'Camera 05',
-            time: '16:13',
-            isRead: true,
-            onTap: () {},
-          ),
-
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Motion detected',
-            subtitle: 'Camera 03',
-            time: '14:23',
-            isRead: true,
-            onTap: () {},
-
-          ),
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Crime detected',
-            subtitle: 'Camera 05',
-            time: '16:13',
-            isRead: false,
-            onTap: () {},
-          ),
-
-          Padding(
-            padding: const EdgeInsets.only(top: 15, left: 17),
-            child: Text(
-              'Older',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ),
-
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Motion detected',
-            subtitle: 'Camera 03',
-            time: '14:23',
-            isRead: true,
-            onTap: () {},
-          ),
-
-          NotificationTile(
-            key: UniqueKey(),
-            title: 'Crime detected',
-            subtitle: 'Camera 05',
-            time: '16:13',
-            isRead: true,
-            onTap: () {},
-          ),
-
-
-        ],
+                return NotificationTile(
+                  title: title,
+                  subtitle: subtitle,
+                  time: time,
+                  onTap: () {
+                    // what to do on tap
+                  },
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
 }
 
 class NotificationTile extends StatelessWidget {
-  // final Key key;
   final String title;
   final String subtitle;
   final String time;
-  final bool isRead;
   final VoidCallback onTap;
 
   const NotificationTile({
-    required Key key,
     required this.title,
     required this.subtitle,
     required this.time,
-    required this.isRead,
     required this.onTap,
-  }): super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -158,51 +100,7 @@ class NotificationTile extends StatelessWidget {
       title: Text(title),
       subtitle: Text(subtitle),
       trailing: Text(time),
-      leading: Icon(                                                                      // trailing Displays the time on the right side of the tile 
-        isRead ? Icons.notifications_off : Icons.notifications,
-        color: isRead ? Colors.grey : Colors.purple,                                  // if notification read turn icon to gray
-      ),
       onTap: onTap,
-    );
-  }
-}
-
-class FilterButtons extends StatelessWidget {
-  final String selectedFilter;
-  final Function(String) onFilterSelected;
-
-  const FilterButtons({
-    Key? key,
-    required this.selectedFilter,
-    required this.onFilterSelected,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      onSelected: onFilterSelected,
-      itemBuilder: (BuildContext context) => [
-        PopupMenuItem<String>(
-          value: 'All',
-          child: Text('All'),
-        ),
-        PopupMenuItem<String>(
-          value: 'Unread',
-          child: Text('Unread'),
-        ),
-        PopupMenuItem<String>(
-          value: 'Motion Detection',
-          child: Text('Motion Detection'),
-        ),
-        PopupMenuItem<String>(
-          value: 'Crime Detection',
-          child: Text('Crime Detection'),
-        ),
-      ],
-      child: Text(
-        selectedFilter,
-        style: TextStyle(color: Colors.white),
-      ),
     );
   }
 }
