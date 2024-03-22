3bb5997a139b6d052b15dce0187926af3a3403c0
Tufayl <tufaylasaf@gmail.com>
2024-03-22 15:10:39 +0530

    Merge branch 'main' of https://github.com/AlthafNazeel/R.A.V.E.N

--- raven_frontend/lib/Pages/add_device.dart
+++ raven_frontend/lib/Pages/add_device.dart
@@ -11,22 +11,6 @@ void main() {
 MyTextField(String label, String hint) {
   // @override
   // Widget build(BuildContext context) {
-<<<<<<< HEAD
-  return TextField(
-    style: TextStyle(color: const Color.fromARGB(255, 209, 209, 209)),
-    decoration: InputDecoration(
-      labelText: label,
-      hintText: hint,
-      labelStyle: TextStyle(color: Colors.white),
-      hintStyle: TextStyle(color: Colors.grey),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(
-            50.0), // Adjust the radius as needed to make it rounded
-      ),
-    ),
-  );
-  SizedBox(height: 16);
-=======
     return TextField(
       style: const TextStyle(color: Color.fromARGB(255, 209, 209, 209)),
       decoration: InputDecoration(
@@ -40,7 +24,6 @@ MyTextField(String label, String hint) {
       ),
     );
     const SizedBox(height: 16);
->>>>>>> a185b81c08c93d364e8e03663ed78d1ce39e6a87
   // }
 }
 
@@ -72,16 +55,6 @@ class AddDevice extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 41, 29, 46),
       ),
       body: Container(
-<<<<<<< HEAD
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 41, 29, 46),
-              Color.fromARGB(255, 54, 36, 63)
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-=======
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [Color.fromARGB(255, 41, 29, 46), Color.fromARGB(255, 54, 36, 63)],
@@ -165,7 +138,6 @@ class AddDevice extends StatelessWidget {
                 ),
               ],
             ),
->>>>>>> a185b81c08c93d364e8e03663ed78d1ce39e6a87
           ),
         ),
         child: Padding(
