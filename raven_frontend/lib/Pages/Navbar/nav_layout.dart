d2103bec600d0ba4b1da494e9194a482070d0886
Oshadhi <oshadhirajakaruna2005@gmail.com>
2024-03-24 05:51:43 +0530

    new navigation bar and linking to signin

--- raven_frontend/lib/Pages/Navbar/nav_layout.dart
+++ raven_frontend/lib/Pages/Navbar/nav_layout.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/material.dart';
-import 'package:raven_frontend/Pages/Navbar/navbar.dart';
-import 'package:raven_frontend/Pages/Navbar/navigation_bloc.dart';
-//import 'package:raven_frontend/Pages/homePage.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:raven_frontend/Pages/Navbar/navbar.dart';
+// import 'package:raven_frontend/Pages/Navbar/navigation_bloc.dart';
+// //import 'package:raven_frontend/Pages/homePage.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NavBarLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocProvider<NavigationBloc>(
-        create: (context) => NavigationBloc(),
-        child: Stack(
-          children: <Widget>[
-            BlocBuilder<NavigationBloc, NavigationStates>(
-              builder: (context, navigationState) {
-                return navigationState as Widget;
-              },
-            ),
-            NavBar(),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class NavBarLayout extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: BlocProvider<NavigationBloc>(
+//         create: (context) => NavigationBloc(),
+//         child: Stack(
+//           children: <Widget>[
+//             BlocBuilder<NavigationBloc, NavigationStates>(
+//               builder: (context, navigationState) {
+//                 return navigationState as Widget;
+//               },
+//             ),
+//             NavBar(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- raven_frontend/lib/Pages/Navbar/navigation_bloc.dart
+++ raven_frontend/lib/Pages/Navbar/navigation_bloc.dart
@@ -1,33 +1,33 @@
-import 'package:bloc/bloc.dart';
-import 'package:raven_frontend/Pages/Notifications.dart';
-import 'package:raven_frontend/Pages/add_device.dart';
-import 'package:raven_frontend/Pages/homePage.dart';
+// import 'package:bloc/bloc.dart';
+// import 'package:raven_frontend/Pages/Notifications.dart';
+// import 'package:raven_frontend/Pages/add_device.dart';
+// import 'package:raven_frontend/Pages/homePage.dart';
 
-enum NavigationEvents {
-  HomePageClickedEvent,
-  NotificationClickedEvent,
-  AddDeviceClickedEvent,
-}
+// enum NavigationEvents {
+//   HomePageClickedEvent,
+//   NotificationClickedEvent,
+//   AddDeviceClickedEvent,
+// }
 
-abstract class NavigationStates {}
+// abstract class NavigationStates {}
 
-class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
-  NavigationBloc(NavigationStates initialState) : super(initialState);
+// class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
+//   NavigationBloc(NavigationStates initialState) : super(initialState);
 
-  NavigationStates get initialState => HomePage();
+//   NavigationStates get initialState => HomePage();
 
-  //@override
-  Stream<NavigationStates> mapEventToState(NavigationEvents event) async* {
-    switch (event) {
-      case NavigationEvents.HomePageClickedEvent:
-        yield HomePage();
-        break;
-      case NavigationEvents.NotificationClickedEvent:
-        yield NotificationsPageE();
-        break;
-      case NavigationEvents.AddDeviceClickedEvent:
-        yield AddDevice();
-        break;
-    }
-  }
-}
+//   //@override
+//   Stream<NavigationStates> mapEventToState(NavigationEvents event) async* {
+//     switch (event) {
+//       case NavigationEvents.HomePageClickedEvent:
+//         yield HomePage();
+//         break;
+//       case NavigationEvents.NotificationClickedEvent:
+//         yield NotificationsPageE();
+//         break;
+//       case NavigationEvents.AddDeviceClickedEvent:
+//         yield AddDevice();
+//         break;
+//     }
+//   }
+// }
