a044c70cc5ff31bc18e6056b37aeec63f8503ef2
Oshadhi <oshadhirajakaruna2005@gmail.com>
2024-03-25 09:37:24 +0530

    navigation menu app bar correction

--- raven_frontend/lib/navMenu.dart
+++ raven_frontend/lib/navMenu.dart
@@ -1,10 +1,60 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:raven_frontend/Pages/Notifications.dart';
+// import 'package:raven_frontend/Pages/SettingsPage.dart';
+// import 'package:raven_frontend/Pages/add_device.dart';
+// import 'package:raven_frontend/Pages/homePage.dart';
+
+// class NavMenu extends StatelessWidget {
+//   const NavMenu({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = Get.put(NavigationController());
+
+//     return Scaffold(
+//       bottomNavigationBar: Obx(
+//         () => NavigationBar(
+//           height: 80,
+//           elevation: 0,
+//           selectedIndex: controller.selectedIndex.value,
+//           onDestinationSelected: (index) =>
+//               controller.selectedIndex.value = index,
+//           destinations: const [
+//             NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+//             NavigationDestination(icon: Icon(Icons.add), label: 'Add Device'),
+//             NavigationDestination(
+//                 icon: Icon(Icons.notifications), label: 'Notifications'),
+//             NavigationDestination(
+//                 icon: Icon(Icons.settings), label: 'Settings'),
+//           ],
+//         ),
+//       ),
+//       body: Obx(() => controller.screens[controller.selectedIndex.value]),
+//     );
+//   }
+// }
+
+// class NavigationController extends GetxController {
+//   final Rx<int> selectedIndex = 0.obs;
+//   final screens = [
+//     HomePage(),
+//     AddDevice(),
+//     NotificationsPageE(),
+//     SettingsPage(),
+//   ];
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:raven_frontend/Pages/Notifications.dart';
 import 'package:raven_frontend/Pages/SettingsPage.dart';
 import 'package:raven_frontend/Pages/add_device.dart';
 import 'package:raven_frontend/Pages/homePage.dart';
+
+// Assume you have NavigationDestination and NavigationBar widgets defined elsewhere
 
 class NavMenu extends StatelessWidget {
   const NavMenu({Key? key}) : super(key: key);
@@ -31,7 +81,13 @@ class NavMenu extends StatelessWidget {
           ],
         ),
       ),
-      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+      body: Column(
+        children: [
+          Expanded(
+            child: Obx(() => controller.screens[controller.selectedIndex.value]),
+          ),
+        ],
+      ),
     );
   }
 }
